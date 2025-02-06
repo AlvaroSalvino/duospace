569543ab4fb74d1ff61ff739ac8d00c43311bd9f
Alvaro Salvino <alvaro.salvino@gmail.com>
2025-02-06 13:34:04 -0300

    .....

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Perfil
+from .models import *
 
 class PerfilAdmin(admin.ModelAdmin):
     list_display = ('nome', 'telefone', 'email', 'fonte', 'cor', 'background', 'usuario', 'imagem_perfil', 'imagem_capa')
@@ -8,4 +8,16 @@ class PerfilAdmin(admin.ModelAdmin):
     readonly_fields = ('imagem_perfilURL', 'imagem_capaURL')
     filter_horizontal = ('contatos',)
 
+class ConviteAdmin(admin.ModelAdmin):
+    list_display = ('solicitante', 'convidado')
+    search_fields = ('solicitante__user__username', 'convidado__user__username')
+    list_filter = ('solicitante', 'convidado')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'perfil', 'data_postagem')
+    search_fields = ('titulo', 'perfil__user__username')
+    list_filter = ('data_postagem', 'perfil')
+    
 admin.site.register(Perfil, PerfilAdmin)
+admin.site.register(Convite, ConviteAdmin)
+admin.site.register(Post, PostAdmin)
--- perfil/models.py
+++ perfil/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+import pillow_heif
+import os
 
 
 class Perfil(models.Model):
@@ -40,6 +43,26 @@ class Perfil(models.Model):
         except:
             url = ''
         return url
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem_perfil:
+            img_path = self.imagem_perfil.path
+            if img_path.lower().endswith(".heic"):
+                heif_image = pillow_heif.open_heif(img_path)
+                img = Image.frombytes(heif_image.mode, heif_image.size, heif_image.data)
+                
+                # Converter para .jpg
+                new_path = os.path.splitext(img_path)[0] + ".jpg"
+                img.save(new_path, format="JPEG")
+
+                # Atualizar o campo imagem_perfil
+                self.imagem_perfil.name = os.path.basename(new_path)
+                self.save()  # Salvar novamente para atualizar o caminho da imagem
+
+                # Remover a imagem .heic original
+                os.remove(img_path)
 
 class Convite(models.Model):
     solicitante = models.ForeignKey(Perfil,on_delete=models.CASCADE,related_name='convites_feitos' )
@@ -67,3 +90,23 @@ class Post(models.Model):
         except:
             url = ''
         return url
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem:
+            img_path = self.imagem.path
+            if img_path.lower().endswith(".heic"):
+                heif_image = pillow_heif.open_heif(img_path)
+                img = Image.frombytes(heif_image.mode, heif_image.size, heif_image.data)
+                
+                # Converter para .jpg
+                new_path = os.path.splitext(img_path)[0] + ".jpg"
+                img.save(new_path, format="JPEG")
+
+                # Atualizar o campo imagem
+                self.imagem.name = os.path.basename(new_path)
+                self.save()  # Salvar novamente para atualizar o caminho da imagem
+
+                # Remover a imagem .heic original
+                os.remove(img_path)
